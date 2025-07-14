ec9aec5578192697d6603078f33eaf903b9f458c
LOVISH THUKRAL <lovishthakural66@gmail.com>
2025-07-14 15:45:19 +0530

    Update user.controller.js

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -50,8 +50,8 @@ export const signupUser = async (req, res) => {
         })
 
 
-        const savedUser = await newuser.save();
-        res.status(201).json({ message: "User created successfully", user: savedUser });
+        const savedUser = await newuser.save().select('username');
+        res.status(200).json({ message: "User created successfully", user: savedUser });
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Error creating user", error: error });
