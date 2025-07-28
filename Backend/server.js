5b479fe767299c82235074cad1b198fd4a257062
Lovish Thukral <thelaviations@yahoo.com>
2025-07-29 00:43:22 +0530

    h

--- Backend/server.js
+++ Backend/server.js
@@ -11,63 +11,55 @@ import connectMedia from './config/mediaServerConfig.js';
 import Chatrouter from './routes/chat.route.js';
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
-import { Socket } from 'node:dgram';
-
 
 const server = express();
-
 dotenv.config();
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const app = createServer(server)
+const app = createServer(server);
 
 const io = new Server(app, {
-    cors: {
-        origin: '*',
-        methods: ["GET", "POST"]
-    }
-})
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
 
 const onlineuser = new Map();
 
-io.on('connection', (Socket) => {
-    Socket.on("register", (userId) => {
-        onlineuser.set(userId, Socket.id);
-    })
+io.on('connection', (socket) => {
+  socket.on('register', (userId) => {
+    onlineuser.set(userId, socket.id);
+  });
 
-    Socket.on('privatemsg', ({ from, toString, text }) => {
-        const target = onlineuser.get(toString);
-        if (target) {
-            io.to(target).emit('privatemsg', { from, text })
-        }
-    })
+  socket.on('privatemsg', ({ from, to, text }) => {
+    const target = onlineuser.get(to);
+    if (target) {
+      io.to(target).emit('privatemsg', { from, text });
+    }
+  });
 
-    Socket.on('disconnect', () => {
-        for (const [userId, sockId] of onlineuser.entries()) {
-            if (sockId === Socket.id) {
-                onlineuser.delete(userId);
-                break;
-            }
-        }
-    })
-})
+  socket.on('disconnect', () => {
+    for (const [userId, sockId] of onlineuser.entries()) {
+      if (sockId === socket.id) {
+        onlineuser.delete(userId);
+        break;
+      }
+    }
+  });
+});
 
 server.use(cookieParser());
 server.use(express.json());
-server.use(express.urlencoded({ extended: true }))
-
-
-
+server.use(express.urlencoded({ extended: true }));
 server.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 server.use('/api/user', userRouter);
-server.use("/api/post", postRouter);
-server.use("/api/chat", Chatrouter)
+server.use('/api/post', postRouter);
+server.use('/api/chat', Chatrouter);
 
 app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running http://localhost:${process.env.PORT}`);
-    connectDB();
-    connectMedia();
+  console.log(`Server is running http://localhost:${process.env.PORT}`);
+  connectDB();
+  connectMedia();
 });
